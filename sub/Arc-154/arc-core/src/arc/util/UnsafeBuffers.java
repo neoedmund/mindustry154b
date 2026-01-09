89ff7030076f3aa568628f353fb11090a3a7f1f2
neoe <e@6610199.xyz>
2026-01-09 11:46:02 +0900

    tweak

--- sub/Arc-154/arc-core/src/arc/util/UnsafeBuffers.java
+++ sub/Arc-154/arc-core/src/arc/util/UnsafeBuffers.java
@@ -1,4 +1,5 @@
 package arc.util;
+
 import java.lang.reflect.Field;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
@@ -8,8 +9,9 @@ import java.nio.ByteOrder;
  * neoe: no more unsafe
  */
 public class UnsafeBuffers {
-	public static final Field address, capacity;
-	public static final boolean failed = false;
+	final public static Field address, capacity;
+
+	final public static boolean failed = false;
 
 	static {
 		try {
@@ -17,34 +19,34 @@ public class UnsafeBuffers {
 			address.setAccessible(true);
 			capacity = Buffer.class.getDeclaredField("capacity");
 			capacity.setAccessible(true);
-		} catch (NoSuchFieldException e) {
+		} catch (Exception e) {
 			throw new AssertionError(e);
 		}
 	}
+
 	public static ByteBuffer wrapAddress(long addr, int length) {
 		ByteBuffer bb = ByteBuffer.allocateDirect(0).order(ByteOrder.nativeOrder());
 		try {
 			address.setLong(bb, addr);
 			capacity.setInt(bb, length);
 			bb.clear();
+			return bb;
 		} catch (IllegalAccessException e) {
 			throw new AssertionError(e);
 		}
-		return bb;
 	}
+
 	public static long getAddr(Buffer bb) {
 		try {
 			return address.getLong(bb);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return 0;
+		} catch (IllegalAccessException e) {
+			throw new AssertionError(e);
 		}
 	}
-	public static void copy(Buffer src, int srcPos, Buffer dst, int dstPos, int length){
-		ByteBuffer a = wrapAddress(getAddr(src)+srcPos,length);
-		ByteBuffer b = wrapAddress(getAddr(dst)+dstPos,length);
-        b.put(a);
-    }
-	public static void checkInit() {
+
+	public static void copy(Buffer src, int srcPos, Buffer dst, int dstPos, int length) {
+		ByteBuffer a = wrapAddress(getAddr(src) + srcPos, length);
+		ByteBuffer b = wrapAddress(getAddr(dst) + dstPos, length);
+		b.put(a);
 	}
 }
